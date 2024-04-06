f36dda611dbf0e98307cc8d71fe5406a8429644c
Bart Czernicki <bartczernicki@outlook.com>
2024-04-06 15:53:48 -0400

    Added Brier Score to the processing options

--- AzureOpenAILogProbs/Program.cs
+++ AzureOpenAILogProbs/Program.cs
@@ -116,7 +116,7 @@ namespace AzureOpenAILogProbs
                 new Question{ Number = 5, EnoughInformationInProvidedContext = false, QuestionText = "Who are the Boston Red Sox?" }, 
                 new Question{ Number = 6, EnoughInformationInProvidedContext = true, QuestionText = "Where the Mets were a bad team in the 1960s?" },
                 new Question{ Number = 7, EnoughInformationInProvidedContext = true, QuestionText = "Do the Mets uniforms include the colors blue and orange?" },
-                new Question{ Number = 8, EnoughInformationInProvidedContext = false, QuestionText = "Are the there only 2 colors onm the Mets uniform?" },
+                new Question{ Number = 8, EnoughInformationInProvidedContext = false, QuestionText = "Are the there only 2 colors on the Mets uniform?" },
                 new Question{ Number = 9, EnoughInformationInProvidedContext = false, QuestionText = "Do you think the Mets were a historically very good team?" },
                 new Question{ Number = 10, EnoughInformationInProvidedContext = true, QuestionText = "Did the Mets have a winning season in their inaugural season of play?" },
                 new Question{ Number = 11, EnoughInformationInProvidedContext = false, QuestionText = "Has Steve Cohen been the longest-serving owner of the Mets?" },
